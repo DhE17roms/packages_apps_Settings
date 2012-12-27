fbee94ceb2a5ed4c5a201f499a3bc54263e4f127
Daryn Emke <dhemke17@Daryn-Emkes-MacBook-Pro.local>
2012-12-27 08:47:56 -0500

    Initial Commit

---
+++ src/com/android/settings/warped/UserInterface.java
@@ -0,0 +1,112 @@
+package com.android.settings.warped;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceGroup;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.text.Spannable;
+import android.view.LayoutInflater;
+import android.widget.EditText;
+
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.R;
+
+public class UserInterface extends SettingsPreferenceFragment {
+	
+    public static final String TAG = "UserInterface";
+	
+    private static final String PREF_STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
+    private static final String PREF_CUSTOM_CARRIER_LABEL = "custom_carrier_label";
+	private static final String PREF_USE_ALT_RESOLVER = "use_alt_resolver";
+	
+    CheckBoxPreference mStatusBarNotifCount;
+	CheckBoxPreference mUseAltResolver;
+    Preference mCustomLabel;
+	
+    String mCustomLabelText = null;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(R.string.user_interface_title);
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.user_interface);
+		
+        mStatusBarNotifCount = (CheckBoxPreference) findPreference(PREF_STATUS_BAR_NOTIF_COUNT);
+        mStatusBarNotifCount.setChecked(Settings.System.getBoolean(mContext
+																   .getContentResolver(), Settings.System.STATUSBAR_NOTIF_COUNT,
+																   false));
+		
+        mCustomLabel = findPreference(PREF_CUSTOM_CARRIER_LABEL);
+        updateCustomLabelTextSummary();
+		
+		mUseAltResolver = (CheckBoxPreference) findPreference(PREF_USE_ALT_RESOLVER);
+		mUseAltResolver.setChecked(Settings.System.getBoolean(mContext.getContentResolver(),
+								  Settings.System.ACTIVITY_RESOLVER_USE_ALT, false));
+		
+    }
+	
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
+										 final Preference preference) {
+        if (preference == mStatusBarNotifCount) {
+            Settings.System.putBoolean(mContext.getContentResolver(),
+									   Settings.System.STATUSBAR_NOTIF_COUNT,
+									   ((CheckBoxPreference) preference).isChecked());
+            return true;
+		} else if (preference == mUseAltResolver) {
+			Settings.System.putBoolean(getActivity().getContentResolver(),
+				Settings.System.ACTIVITY_RESOLVER_USE_ALT,
+			    isCheckBoxPreferenceChecked(preference));
+			return true;
+        } else if (preference == mCustomLabel) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+			
+            alert.setTitle(R.string.custom_carrier_label_title);
+            alert.setMessage(R.string.custom_carrier_label_explain);
+			
+            // Set an EditText view to get user input
+            final EditText input = new EditText(getActivity());
+            input.setText(mCustomLabelText != null ? mCustomLabelText : "");
+            alert.setView(input);
+			
+            alert.setPositiveButton(getResources().getString(R.string.ok), new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    String value = ((Spannable) input.getText()).toString();
+                    Settings.System.putString(getActivity().getContentResolver(),
+											  Settings.System.CUSTOM_CARRIER_LABEL, value);
+                    updateCustomLabelTextSummary();
+                    Intent i = new Intent();
+                    i.setAction("com.android.settings.warped.LABEL_CHANGED");
+                    mContext.sendBroadcast(i);
+                }
+            });
+            alert.setNegativeButton(getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    // Canceled.
+                }
+            });
+			
+            alert.show();
+        }
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+	
+    private void updateCustomLabelTextSummary() {
+        mCustomLabelText = Settings.System.getString(getActivity().getContentResolver(),
+													 Settings.System.CUSTOM_CARRIER_LABEL);
+        if (mCustomLabelText == null || mCustomLabelText.length() == 0) {
+            mCustomLabel.setSummary(R.string.custom_carrier_label_notset);
+        } else {
+            mCustomLabel.setSummary(mCustomLabelText);
+        }
+    }
+}
