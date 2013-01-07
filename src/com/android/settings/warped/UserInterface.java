a6391d97f2b2813a5f2d5e1ab533c769ab32b962
dhemke17 <dhemke17@gmail.com>
2013-01-07 13:06:34 -0500

    Quick Settings....Volume Settings....Prolly more...

--- src/com/android/settings/warped/UserInterface.java
+++ src/com/android/settings/warped/UserInterface.java
@@ -18,6 +18,7 @@ import android.widget.EditText;
 
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.R;
+import com.android.settings.util.Helpers;
 
 public class UserInterface extends SettingsPreferenceFragment {
 	
@@ -26,10 +27,18 @@ public class UserInterface extends SettingsPreferenceFragment {
     private static final String PREF_STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
     private static final String PREF_CUSTOM_CARRIER_LABEL = "custom_carrier_label";
 	private static final String PREF_USE_ALT_RESOLVER = "use_alt_resolver";
+	private static final String PREF_VIBRATE_NOTIF_EXPAND = "vibrate_notif_expand";
+	private static final String PREF_VOLUME_ROCKER_WAKE = "volume_rocker_wake";
+	private static final String PREF_VOLUME_MUSIC = "volume_music_controls";
+	private static final String PREF_ENABLE_VOLUME_OPTIONS = "enable_volume_options";
 	
+	Preference mCustomLabel;
     CheckBoxPreference mStatusBarNotifCount;
 	CheckBoxPreference mUseAltResolver;
-    Preference mCustomLabel;
+	CheckBoxPreference mVibrateOnExpand;
+	CheckBoxPreference mVolumeRockerWake;
+	CheckBoxPreference mVolumeMusic;
+	CheckBoxPreference mEnableVolumeOptions;
 	
     String mCustomLabelText = null;
 	
@@ -52,6 +61,22 @@ public class UserInterface extends SettingsPreferenceFragment {
 		mUseAltResolver.setChecked(Settings.System.getBoolean(mContext.getContentResolver(),
 								  Settings.System.ACTIVITY_RESOLVER_USE_ALT, false));
 		
+		mEnableVolumeOptions = (CheckBoxPreference) findPreference(PREF_ENABLE_VOLUME_OPTIONS);
+		mEnableVolumeOptions.setChecked(Settings.System.getInt(getActivity().getContentResolver(),
+								   Settings.System.ENABLE_VOLUME_OPTIONS, 0) == 1);
+		
+		mVibrateOnExpand = (CheckBoxPreference) findPreference(PREF_VIBRATE_NOTIF_EXPAND);
+        mVibrateOnExpand.setChecked(Settings.System.getBoolean(mContext.getContentResolver(),
+								   Settings.System.VIBRATE_NOTIF_EXPAND, true));
+		
+        mVolumeRockerWake = (CheckBoxPreference) findPreference(PREF_VOLUME_ROCKER_WAKE);
+        mVolumeRockerWake.setChecked(Settings.System.getBoolean(mContext
+									.getContentResolver(), Settings.System.VOLUME_WAKE_SCREEN, false));
+
+        mVolumeMusic = (CheckBoxPreference) findPreference(PREF_VOLUME_MUSIC);
+        mVolumeMusic.setChecked(Settings.System.getBoolean(getActivity().getContentResolver(),
+								   Settings.System.VOLUME_MUSIC_CONTROLS, false));
+		
     }
 	
     @Override
@@ -66,6 +91,28 @@ public class UserInterface extends SettingsPreferenceFragment {
 			Settings.System.putBoolean(getActivity().getContentResolver(),
 				Settings.System.ACTIVITY_RESOLVER_USE_ALT,
 			    isCheckBoxPreferenceChecked(preference));
+			return true;
+		} else if (preference == mVibrateOnExpand) {
+            Settings.System.putBoolean(getActivity().getContentResolver(),
+									   Settings.System.VIBRATE_NOTIF_EXPAND,
+									   ((CheckBoxPreference) preference).isChecked());
+            Helpers.restartSystemUI();
+            return true;
+        } else if (preference == mVolumeRockerWake) {
+            Settings.System.putBoolean(getActivity().getContentResolver(),
+									   Settings.System.VOLUME_WAKE_SCREEN,
+									   ((CheckBoxPreference) preference).isChecked());
+            return true;
+        } else if (preference == mVolumeMusic) {
+			
+            Settings.System.putBoolean(getActivity().getContentResolver(),
+									   Settings.System.VOLUME_MUSIC_CONTROLS,
+									   ((CheckBoxPreference) preference).isChecked());
+            return true;
+		} else  if (preference == mEnableVolumeOptions) {
+			boolean checked = ((CheckBoxPreference) preference).isChecked();
+			Settings.System.putInt(getActivity().getContentResolver(),
+								   Settings.System.ENABLE_VOLUME_OPTIONS, checked ? 1 : 0);
 			return true;
         } else if (preference == mCustomLabel) {
             AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
